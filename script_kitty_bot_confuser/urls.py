14d8bbf067c7777993d9876f6deaeca9f07efe90
Thomas Levine <thomas.levine@gmail.com>
2011-11-21 18:47:20 -0500

    readme and start

--- script_kitty_bot_confuser/urls.py
+++ script_kitty_bot_confuser/urls.py
@@ -1,5 +1,21 @@
 from django.conf.urls.defaults import patterns, include, url
 from views import confuse 
 
+#Confuse bots on these URLs
 urlpatterns = patterns('',
-  url(r'^api/(?P<domainname>[^/]*)/$',confuse, name='pageview'),
+  #Languages
+  url(r'.php$',confuse),
+  url(r'.cgi$',confuse),
+  url(r'.rb$',confuse),
+  url(r'.aspx$',confuse),
+  url(r'.do$',confuse),
+  url(r'$',confuse),
+
+  #Version control
+  url(r'^.svn',confuse),
+  url(r'^.git',confuse),
+  url(r'^.hg',confuse),
+
+  #Administration software
+  url(r'$',confuse),
+)
--- script_kitty_bot_confuser/views.py
+++ script_kitty_bot_confuser/views.py
@@ -1,1 +1,9 @@
-# Create your views here.
+def silence(request):
+  """Don't do anything!"""
+  pass
+
+def wild_goose_chase(request):
+  """Send them somewhere else."""
+  return HttpResponseRedirect('http://google.com')
+
+confuse=silence
